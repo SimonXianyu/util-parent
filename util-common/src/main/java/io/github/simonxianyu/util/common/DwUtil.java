3776bfbbb4533c76b2a5abd37dc04194b532b016
Simon Xianyu <hpxianyu@paypos.ca>
2015-10-08 15:37:29 +0800

    add static safe method to simplify null checking in  for-each

--- util-common/src/main/java/io/github/simonxianyu/util/common/DwUtil.java
+++ util-common/src/main/java/io/github/simonxianyu/util/common/DwUtil.java
@@ -2,12 +2,20 @@ package io.github.simonxianyu.util.common;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
 /**
  *
  * Created by Simon Xianyu on 2015/9/16 0016.
  */
 public class DwUtil {
+
+  @SuppressWarnings("unchecked")
+  public static <T> List<T> safe(List<T> a) {
+    return a == null ? Collections.EMPTY_LIST : a;
+  }
+
   public static String stringValue(Object obj) {
     return null == obj ? null : obj.toString();
   }
